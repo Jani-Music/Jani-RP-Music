796440e80bc84711517141040f24461bb2d7146f
Jani-Music <hahahsygs395@gmail.com>
2025-11-08 00:04:04 +0530

    Update couples.py

--- SONALI_MUSIC/plugins/tools/couples.py
+++ SONALI_MUSIC/plugins/tools/couples.py
@@ -15,7 +15,7 @@ POLICE = [
     [
         InlineKeyboardButton(
             text="• ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ •",
-            url=f"https://t.me/MayuriMusicBot?startgroup=true",
+            url=f"https://t.me/SelfishMusicBot?startgroup=true",
         ),
     ],
 ]
